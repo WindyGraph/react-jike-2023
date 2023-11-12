ec6f27529df19b2fca49ccf9193ea04a9c4f0283
WindyGraph <137336036@qq.com>
2023-11-12 21:41:53 +0800

    Add token removal and redirect on 401 error

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 // 1. 根域名配置
 // 2. 超时事件
 // 3. 请求拦截器 / 响应拦截器
-import { getToken } from './token'
+import { getToken, removeToken } from './token'
+import router from '@/router'
 
 const request = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0',
@@ -36,6 +37,11 @@ request.interceptors.response.use(
   (error) => {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
+    if (error?.response?.status === 401) {
+      removeToken()
+      router.navigate('/login')
+      window.location.reload()
+    }
     return Promise.reject(error)
   }
 )
