87c09fd7852a8a39369429c7adff42aa9472facb
WindyGraph <137336036@qq.com>
2023-11-12 20:27:29 +0800

    Add normalize.css and update layout styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import 'normalize.css'
 
 import '@/index.scss'
 import router from '@/router'
