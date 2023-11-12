fd746d7a62b88a3d44d37163561391185e2dd2f5
WindyGraph <137336036@qq.com>
2023-11-12 20:21:20 +0800

    Add AuthRoute component and update imports

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
-import './index.scss'
-import router from './router'
-import store from './store'
+import '@/index.scss'
+import router from '@/router'
+import store from '@/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
