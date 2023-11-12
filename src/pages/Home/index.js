018998ec414f7b6fa3e85086046e1f736e1485d3
WindyGraph <137336036@qq.com>
2023-11-12 21:59:41 +0800

    Add echarts library and implement a bar chart on Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,37 @@
+import * as echarts from 'echarts'
+import { useEffect, useRef } from 'react'
+
 const Home = () => {
-  return <div>Home</div>
+  const chartRef = useRef('')
+  useEffect(() => {
+    const chartDom = chartRef.current
+    const myChart = echarts.init(chartDom)
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['Vue', 'Angular', 'React'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: 'bar',
+        },
+      ],
+    }
+
+    option && myChart.setOption(option)
+  }, [])
+  return (
+    <div>
+      <div
+        ref={chartRef}
+        style={{ width: '500px', height: '400px' }}
+      />
+    </div>
+  )
 }
 
 export default Home
