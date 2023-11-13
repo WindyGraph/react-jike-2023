bd37c487f51ecd787b1df88bd87965a95b2d9ca4
WindyGraph <137336036@qq.com>
2023-11-13 15:14:13 +0800

    Add lazy loading and suspense to router components

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
+import { Suspense, lazy } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 
 import { AuthRoute } from '@/components/AuthRoute'
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
-import Home from '@/pages/Home'
-import Article from '@/pages/Article'
-import Publish from '@/pages/Publish'
+
+const Home = lazy(() => import('@/pages/Home'))
+const Article = lazy(() => import('@/pages/Article'))
+const Publish = lazy(() => import('@/pages/Publish'))
 
 const router = createBrowserRouter([
   {
@@ -18,15 +20,27 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />,
+        element: (
+          <Suspense fallback={'加载中'}>
+            <Home />
+          </Suspense>
+        ),
       },
       {
         path: '/article',
-        element: <Article />,
+        element: (
+          <Suspense fallback={'加载中'}>
+            <Article />
+          </Suspense>
+        ),
       },
       {
         path: '/publish',
-        element: <Publish />,
+        element: (
+          <Suspense fallback={'加载中'}>
+            <Publish />
+          </Suspense>
+        ),
       },
     ],
   },
